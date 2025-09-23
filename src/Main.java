411e020c17ae6e1f4a8b5bdc09646bd5ffa1f32f
charryp <c.charryp@uniandes.edu.co>
2025-09-23 10:52:05 -0500

    ESTRUCTURA DE SOPORTE DE LA OPCION 1

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static String[] menuOpciones = new String[] {
+				"0- Salir de la aplicacion",
+				"1- Ejecutar la opcion 1\n",
+                "2- Ejecutar la opcion 2\n"};
+    
+    public static final String input(String mensaje) {
+		try {
+			System.out.print(mensaje);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			return reader.readLine();
+		} catch (IOException e) {
+			System.out.println("Error leyendo de la consola");
+			e.printStackTrace();
+		}
+		return null;
+	}
+    public static void mostrarMenu(String[] menuOpciones) {
+		System.out.println("\n");
+		for (String opcion : menuOpciones) {
+			System.out.println(opcion);
+		}
+	}
+
+    public static void main(String args[]){
+        System.out.println("Bienvenido a la aplicacion...   ");
+        System.out.println("--------------------------------");
+        System.out.println("IMPORTANTE: Los archivos de configuracion debe ubicarlos en la carpeta DocsConfiguracion");
+        System.out.println("--------------------------------");
+        System.out.println("A continuacion, las opciones de la aplicacion: ");
+        boolean continuar = true;
+
+        while (continuar) {
+				mostrarMenu(menuOpciones);
+				int seleccion = Integer.parseInt(input("\nSeleccione una opcion: "));
+				System.out.println("\n");
+
+				switch (seleccion) {
+				case 0:
+					input("Desea salir de la App? Presione Enter...");
+					System.out.println("-----------------------------------------------------");
+					System.out.println("Vuelva pronto!!");
+					System.out.println("-----------------------------------------------------");
+					System.exit(0);
+
+				case 1:
+					input("Desea continuar con la opcion 1? Presiones Enter...");
+					//Opcion 1
+					System.out.println("-----------------------------------------------------");
+					System.out.println("La actividad ha sido calificada exitosamente!!");
+					System.out.println("-----------------------------------------------------");
+					break;
+                case 2:
+					input("Desea continuar con la opcion 2? Presiones Enter...");
+					//Opcion 2
+					System.out.println("-----------------------------------------------------");
+					System.out.println("La actividad ha sido calificada exitosamente!!");
+					System.out.println("-----------------------------------------------------");
+					break;
+				default:
+					input("La seleccion no es valida. Presione Enter y vuelva a intentar...");
+					break;
+				}
+
+    }
+
+    
+    
+}
--- src/Opcion1.java
+++ src/Opcion1.java
@@ -1,45 +1,121 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Opcion1 {
+    private int[][] matriz1;
+    private int[][] matriz2;
 
-	/**
-	 * Lee el contenido de un archivo dentro de la carpeta DocsConfiguracion.
-	 * @param nombreArchivo Nombre del archivo dentro de DocsConfiguracion
-	 * @return El contenido del archivo como String
-	 * @throws IOException Si ocurre un error de lectura
-	 */
-	public static String[] leerArchivo(String nombreArchivo) throws IOException {
+    public int[][] getMatriz1() {
+        return matriz1;
+    }
+
+    public void setMatriz1(int[][] matriz1) {
+        this.matriz1 = matriz1;
+    }
+
+    public int[][] getMatriz2() {
+        return matriz2;
+    }
+
+    public void setMatriz2(int[][] matriz2) {
+        this.matriz2 = matriz2;
+    }
+
+    
+	public void ejecutarOpcion1(String ruta) throws IOException{
+        ArrayList<String> info = leerArchivo(ruta);
+        int TP = Integer.parseInt(getTP(info));
+        int NPROC = Integer.parseInt(getNPROC(info));
+        ArrayList<ArrayList<Integer>> tams = getTAMS(info);
+
+        for(int i = 0 ; i < NPROC; i++){
+            System.out.println("Iniciando proceso " + (i + 1));
+        }
+        setMatriz1(generarMatrizRandom(tams[0], tams[1]));
+        setMatriz2(generarMatrizRandom(tams[0], tams[1]));
+        ejecutarSimulador(matriz1, matriz2);
+        //Inicio proceso 2
+        setMatriz1(generarMatrizRandom(tams[2], tams[3]));
+        setMatriz2(generarMatrizRandom(tams[2], tams[3]));
+        ejecutarSimulador(matriz1, matriz2);
+
+    }
+	public static ArrayList<String> leerArchivo(String nombreArchivo) throws IOException {
 		Path ruta = Paths.get("src", "DocsConfiguracion", nombreArchivo);
-		return Files.readString(ruta).split("\n");
+		return new ArrayList<>(Arrays.asList(Files.readString(ruta).split("\n")));
 	}
 
-    public static String getTP(String[] archivo){
+    public static String getTP(ArrayList<String> archivo){
         
-        return archivo[0];
+        return archivo.get(0).replace("TP=", "");
     }
-    public static String getNPROC(String[] archivo){
+    public static String getNPROC(ArrayList<String> archivo){
         
-        return archivo[1];
+        return archivo.get(1).replace("NPROC=", "");
     
     }
 
-    public static String getTAMS(String[] archivo){
-        return archivo[2];
+    public static ArrayList<ArrayList<Integer>> getTAMS(ArrayList<String> archivo){
+        ArrayList<ArrayList<Integer>> allTAMS = new ArrayList<>();
+        for(int i = 2 ; i < archivo.size(); i++){
+            String[] tams = archivo.get(i).replace("TAMS=", "").split(",");
+            for(String tam : tams){
+                String[] dimensiones = tam.split("x");
+                ArrayList<Integer> tamanos = new ArrayList<>();
+                tamanos.add(Integer.parseInt(dimensiones[0]));
+                tamanos.add(Integer.parseInt(dimensiones[1]));
+                allTAMS.add(tamanos);
+            }
+        }
+
+        return allTAMS;
     }
 
-	public void SumadorMatrices(int pnf, int pnc){
+    public static int[][] generarMatrizRandom(int filas, int columnas) {
+    int[][] matriz = new int[filas][columnas];
+    Random rand = new Random();
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = rand.nextInt();
+        }
+    }
+    return matriz;
+    }
+
+	public int[][] sumarMatrices(int pnf, int pnc){
         int filas = pnf;
         int columnas = pnc;
+        int[][] matriz3 = new int[filas][columnas];
 
         for(int i = 0 ; i < filas; i++){
             for(int j = 0; j < columnas; j++){
-                System.out.print("1 ");
+                matriz3[i][j] = matriz1[i][j] + matriz2[i][j];
             }
-            System.out.println();
         }
+        return matriz3;
     }
+
+    public void ejecutarSimulador(int[][] matrizA, int[][] matrizB){
+
+    }
+
+	public void generarArchivos(int numProcesos, int TP, int NF, int NC, int NR, int NP, List<String> direcciones) throws IOException{
+		for(int i = 0 ; i < numProcesos; i++){
+			Path ruta = Paths.get("src", "DocsConfiguracion", "salida" + i + ".txt");
+			String contenido = "Texto a guardar";
+			Files.write(ruta, Integer.toString(TP)+"\\n".getBytes());
+		}
+		
+	}
+
 }
